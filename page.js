d4bf0371b52c36740ee5bfec0fbe612c71bbfffd
Rammohan791 <rammohansingh.cse23@kgikanpur.in>
2025-11-04 10:01:15 +0530

    added index.html

---
+++ page.js
@@ -0,0 +1,39 @@
+	let bntclick=document.getElementById("button");
+    function btn(){
+    window.location.href="kandya.html";}
+	
+let quizzbutton=document.getElementById("quizzebutton");
+function quizzbtn(){
+window.location.href="quizzpage.html";}
+
+let quizzespage=document.getElementById("quizzesopenbtn");
+function quizzpage(){
+window.location.href="quizzes.html";}
+
+let balkandya=document.getElementById("balkand");
+function balkand(){
+window.location.href="balkand.html";}
+
+let ayodhyakandya=document.getElementById("ayodhyakand");
+function ayodhyakand(){
+window.location.href="ayodhyakand.html";}
+
+let c=document.getElementById("aranyakand");
+function aranyakand(){
+window.location.href="aranyakand.html";}	
+	
+let d=document.getElementById("kishkindhakand");
+function kishkindhakand(){
+window.location.href="kishkindhakand.html";}
+
+let e=document.getElementById("sundarkand");
+function sundarkand(){
+window.location.href="sundarkand.html";}
+
+let f=document.getElementById("lankakand");
+function lankakand(){
+window.location.href="lankakand.html";}
+
+let g=document.getElementById("uttarakand");
+function uttarakand(){
+window.location.href="uttarakand.html";}
