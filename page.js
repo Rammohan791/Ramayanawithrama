427541b23fc42c2fc12b9061bbea1e04fc4b1074
Rammohan791 <rammohansingh.cse23@kgikanpur.in>
2025-11-08 00:28:18 +0530

    added index.html

--- page.js
+++ page.js
@@ -45,3 +45,35 @@ window.location.href="whatramayana.html";}
 let b=document.getElementById("aboutramayana");
 function learnabout(){
 window.location.href="aboutramayana.html";}
+
+let p=document.getElementById("b1");
+function p1(){
+window.location.href="birthofram.html";}
+let q=document.getElementById("b2");
+function q1(){
+window.location.href="arrivalofvishwamitra.html";}
+
+let r=document.getElementById("b3");
+function r1(){
+window.location.href="tadkaslaying.html";}
+
+let s=document.getElementById("b4");
+function s1(){
+window.location.href="ahalyasavation.html";}
+
+let t=document.getElementById("b5");
+function t1(){
+window.location.href="breakingofshivabow.html";}
+
+let u=document.getElementById("b6");
+function u1(){
+window.location.href="marriagesitaram.html";}
+
+let v=document.getElementById("b7");
+function v1(){
+window.location.href="parshuramepisode.html";}
+
+let w=document.getElementById("b8");
+function w1(){
+window.location.href="returntoayodhya.html";}
+
