4afe681c8f1ba6197c90033b31ed521a29be61c4
Rammohan791 <rammohansingh.cse23@kgikanpur.in>
2025-11-16 01:42:42 +0530

    added index.html

--- page.js
+++ page.js
@@ -80,3 +80,169 @@ window.location.href="returntoayodhya.html";}
 let cult=document.getElementById("culturepage");
 function cutlurepage(){
 window.location.href="indianculture.html";}
+
+function fun1(){
+window.location.href="rajyaabhishekpreparation.html";}
+
+function fun2(){
+window.location.href="kaikeyboons.html";}
+
+function fun3(){
+window.location.href="ramexile.html";}
+
+function fun4(){
+window.location.href="deathofdasharatha.html";}
+
+function fun5(){
+window.location.href="bharatarrival.html";}
+
+function fun6(){
+window.location.href="bharatgovernance.html";}
+
+function fun7(){
+window.location.href="griefofsumitra.html";}
+
+<!--aranyakand-->
+
+function fun8(){
+window.location.href="entryintofprest.html";}
+
+function fun9(){
+window.location.href="viradhyaslaying.html";}
+
+function fun10(){
+window.location.href="sarabhangaashram.html";}
+
+function fun11(){
+window.location.href="agastyamuni.html";}
+
+function fun12(){
+window.location.href="panchvati.html";}
+
+function fun13(){
+window.location.href="shurpnakha.html";}
+
+function fun14(){
+window.location.href="khardushanvadh.html";}
+
+function fun15(){
+window.location.href="ravalkaagman.html";}
+
+function fun16(){
+window.location.href="jatayu.html";}
+
+<!--kishkindhakand-->
+
+
+function fun17(){
+window.location.href="ramreachmalyavan.html";}
+
+function fun18(){
+window.location.href="meetinghanuman.html";}
+
+function fun19(){
+window.location.href="friendssugriva.html";}
+
+function fun20(){
+window.location.href="balislaying.html";}
+
+function fun21(){
+window.location.href="sugrivacoronation.html";}
+
+function fun22(){
+window.location.href="searchforsita.html";}
+
+<!--kishkindhakand-->
+
+function fun23(){
+window.location.href="hanumanjisamundra.html";}
+
+function fun24(){
+window.location.href="mainakacounse.html";}
+
+function fun25(){
+window.location.href="suasaslaying.html";}
+
+function fun26(){
+window.location.href="simhikavadh.html";}
+
+function fun27(){
+window.location.href="entryintolanka.html";}
+
+
+function fun28(){
+window.location.href="meetingsita.html";}
+
+function fun29(){
+window.location.href="akhshaykumar.html";}
+
+function fun30(){
+window.location.href="lankadahan.html";}
+
+function fun31(){
+window.location.href="sitagivingjwel.html";}
+
+function fun32(){
+window.location.href="returnfromlanka.html";}
+
+<!--lankakand-->
+
+function fun33(){
+window.location.href="bridgebuilding.html";}
+
+function fun34(){
+window.location.href="vanarentrylanka.html";}
+
+function fun35(){
+window.location.href="prahdthavadh.html";}
+
+function fun36(){
+window.location.href="kumbkaranslaying.html";}
+
+function fun37(){
+window.location.href="meghnathvadh.html";}
+
+
+function fun38(){
+window.location.href="ravanvadh.html";}
+
+function fun39(){
+window.location.href="sitaagniparikhsha.html";}
+
+function fun40(){
+window.location.href="dividinglanka.html";}
+
+function fun41(){
+window.location.href="returntoayodhyaafterwin.html";}
+
+<!--uttarkand-->
+
+function fun42(){
+window.location.href="ramcoronation.html";}
+
+function fun43(){
+window.location.href="reignofram.html";}
+
+function fun44(){
+window.location.href="shambukaslaying.html";}
+
+
+function fun45(){
+window.location.href="sitaexile.html";}
+
+function fun46(){
+window.location.href="birthoflavekush.html";}
+
+function fun47(){
+window.location.href="ashwamedhayagya.html";}
+
+function fun48(){
+window.location.href="thelateryearend.html";}
+
+<!--motivation hindi and english-->
+function fun49(){
+window.location.href="motivationchopaihindi.html";}
+
+function nextquizz(){
+window.location.href="motivationchopaienglish.html";}
+
